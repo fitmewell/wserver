f7f1dd0a36b55a3d6ee3f146902b9a98fa15e9f7
fitmewell <t123456>
2017-07-18 14:50:24 +0800

    more ioc type add

--- handler.go
+++ handler.go
@@ -24,10 +24,10 @@ func (h *wHandler) init() {
 	for _, resource := range h.wServer.config.StaticResources {
 		path := resource.Path
 		if strings.HasSuffix(path, "**") {
-			path = path[0 : len(path)-2]
+			path = path[0: len(path)-2]
 		}
 		if strings.HasSuffix(path, "*") {
-			path = path[0 : len(path)-1]
+			path = path[0: len(path)-1]
 		}
 		t := http.StripPrefix(path, http.FileServer(http.Dir(resource.FileLocate)))
 		DebugF("Severing static files: %s %s", path, resource.FileLocate)
@@ -150,7 +150,33 @@ func (h *wHandler) handle(context ServletContext, resp http.ResponseWriter, req 
 				}
 				inputs[i] = t
 			}
-
+		case reflect.String:
+			b, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				return err
+			}
+			inputs[i] = reflect.ValueOf(string(b))
+		case reflect.Slice:
+		case reflect.Array:
+			b, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				return err
+			}
+			inputs[i] = reflect.ValueOf(string(b))
+			switch at.Elem().Kind() {
+			case reflect.Uint8:
+				inputs[i] = reflect.ValueOf(b)
+			}
+		case reflect.Map:
+			err = req.ParseForm()
+			if err != nil {
+				return err
+			}
+			p := map[string][]string{}
+			for fk, fv := range req.Form {
+				p[fk] = fv
+			}
+			inputs[i] = reflect.ValueOf(p)
 		}
 	}
 	outs := v.Call(inputs)
